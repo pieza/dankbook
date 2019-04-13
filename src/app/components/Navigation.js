fb6d499092508c72cc9ebd5a5a11e0a50bd5a1bc
José Andrés Ulloa <julloah940@ulacit.ed.cr>
2019-04-12 18:04:34 -0600

    save changes

--- src/app/components/Navigation.js
+++ src/app/components/Navigation.js
@@ -7,7 +7,7 @@ class Navigation extends Component {
         return (
             <nav className="light-blue darken-4">
                 <div className="container">
-                    <a className="brand-logo" href="/">DankBook</a>
+                    <Link to="/login" className="brand-logo">DankBook</Link>
                     <ul className="right hide-on-med-and-down">
                         <li><Link to="/login"><i className="material-icons left">search</i>Login</Link></li>
                         <li><Link to="/signup"><i className="material-icons left">search</i>Sign up</Link></li>
